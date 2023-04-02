c4d76d46f062ec9672b9271e139115dfb9a8982e
namra <namra@namratha>
2023-04-02 19:15:12 +0530

    First_Commit

---
+++ C2TC/src/com/tnspackage/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.tnspackage;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
