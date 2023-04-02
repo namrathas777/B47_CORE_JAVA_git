2e059cb17eb618aae39b24ffa3806cf56dd81af2
namra <namra@namratha>
2023-04-02 22:38:09 +0530

    third commit

--- C2TC/src/com/tnsexception/defaultdemo.java
+++ C2TC/src/com/tnsexception/defaultdemo.java
@@ -1,5 +1,17 @@
 package com.tnsexception;
 
-public class defaultdemo {
+public class defaultdemo{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str=null;
+		try {
+			str.equals("Hello");
+		}catch(NullPointerException e ) {
+			System.out.println("This is done");
+		}
+		
+
+	}
 
 }
