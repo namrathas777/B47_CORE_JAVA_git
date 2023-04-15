4865f10d8e6e608313c38ab9da9b68ee63e79df3
Namratha <namrathas777@gmail.com>
2023-04-16 00:15:19 +0530

    base overriding

---
+++ C2TC/src/com/tnsoverridding/overiding.java
@@ -0,0 +1,18 @@
+package com.tnsoverridding;
+
+class Base {
+
+void sum () {
+	System.out.println("Sum");
+}
+}
+
+public class overiding extends Base {
+    
+     void sum()
+{
+	System.out.println();
+}
+
+
+}
